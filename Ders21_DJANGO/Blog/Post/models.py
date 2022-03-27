7731bee71e3d10d61ab8bea2e1b39d0ceb930e96
Mehmet DEMİR <mail.mehmetdemir@gmail.com>
2022-03-27 13:58:54 +0300

    Update Ders21

--- Ders21_DJANGO/Blog/Post/models.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from django.db import models
-
-# Create your models here.
-class Blog(models.Model):
-    BlogID = models.AutoField(primary_key=True)
-    BlogTitle = models.CharField(max_length=100)
-    BlogText = models.TextField()
-    BlogPublishDate = models.DateTimeField(default=datetime.now)
